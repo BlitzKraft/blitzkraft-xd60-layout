ac3d0f7ccade2283891d766a684f90669be9563b
BK Bolisetty <bk@blitzkraft.me>
2018-10-02 00:10:36 -0400

    Lot of usability improvements and update README
    * Updated the keymaps (ASCII keyboards) to reflect the actual keycodes in
    the layers.
    
    * Added feature to change underglow color based on the current layer
    state.
    
    * Capslock is control on hold.
    
    * Previously unused key in R4 is mouse clicks (Left click on layer 0 and
    right click on layer 1).
    
    * Arrow keys now properly correspond to PGUP, PGDN, HOME and END - up,
    down, left and right respectively on FN layer.

--- keymap.c
+++ keymap.c
@@ -1,5 +1,4 @@
 #include "xd60.h"
-#include "action_layer.h"
 #define MOUSEKEY_DELAY             16
 #define MOUSEKEY_INTERVAL          0
 #define MOUSEKEY_MAX_SPEED         7
@@ -12,45 +11,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
 /* 0: Base Layer
  * .----------------------------------------------------------------.
- * | Esc | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = | \ | Bksp |
+ * |GrEsc| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = | \ | Bksp |
  * |----------------------------------------------------------------+
  * | Tab  | Q | W | E | R | T | Y | U | I | O | P | [ | ] |    \    |
  * |----------------------------------------------------------------+
- * |  Caps  | A | S | D | F | G | H | J | K | L | ; | ' |   Enter   |
+ * |CL/CTRL | A | S | D | F | G | H | J | K | L | ; | ' |   Enter   |  Caps - CapsLock on Tap, CTRL on hold
  * |----------------------------------------------------------------+
- * |   Shift  | Z | X | C | V | B | N | M | , | . | / |  RShift | ↑ |
+ * |   Shift  | Z | X | C | V | B | N | M | , | . |Shft/Slsh| ↑ |LC |  Right Shift - Shift on hold, Slash on tap, Left click
  * |----------------------------------------------------------------+
- * | LCtrl | LGUI | LAlt |      Space       | RAlt | FN | ← | ↓ | → |
+ * | LCtrl | LGUI | LAlt |    Space/FN      | RAlt |Ctrl| ← | ↓ | → |
  * '----------------------------------------------------------------'
  */
-  KEYMAP(
+  LAYOUT_all(
       KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_NO,    \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSLS,   \
-      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,    \
-      KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_RSFT,  KC_UP,      \
+      CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,    \
+      KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_NO, RSFT_T(KC_SLSH), KC_UP,  KC_MS_BTN1,      \
       KC_LCTL, KC_LGUI, KC_LALT,                          LT(1, KC_SPC),                       KC_RALT, KC_RCTL,     KC_LEFT, KC_DOWN,  KC_RIGHT),
 
 /* 1: Function Layer
  * .----------------------------------------------------------------.
- * | `   |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12| \ | Bksp |
+ * | `   |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12| \ | Del  |
  * |----------------------------------------------------------------+
- * | Tab  | Q | W | E | R | T | Y | U | I | O | P | [ | ] |    \    |
+ * | Tab  |LC |MU |RC | * | T | Y | U | I | O | P | [ | ] |    \    |  Left Click, Mouse Up, Right Click, * programming mode/RESET
  * |----------------------------------------------------------------+
- * |  Caps  | A | S | D | F | G | H | J | K | L | ; | ' |   Enter   |
+ * |  BlAdj |ML |MD |MR | F | G | H | J | K | L | ; | ' |   Enter   |  Backlight adjust; Mouse Left, Mouse Down, Mouse Right - Mouse movements
  * |----------------------------------------------------------------+
- * |   Shift  | Z | X | C | V | B | N | M | , | . | / |  RShift | ↑ |
+ * |   Shift  | Z | ! | ? | V | B | N | M | , | . |Shft/Slsh|PU |RC |  Underglow (!) toggle on/off (?) cycle modes, Right Click
  * |----------------------------------------------------------------+
- * | LCtrl | LGUI | LAlt |      Space       | RAlt | FN | ← | ↓ | → |
+ * | LCtrl | WIN  | LAlt |    Space/FN      | RAlt |Ctrl|HM |PD |END|  Arrow cluster Home, PageUp, PageDown, End
  * '----------------------------------------------------------------'
  */
-  KEYMAP(
+  LAYOUT_all(
       KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,   KC_F14,    \
-      KC_NO,   KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2, RESET,KC_HOME,KC_CALC,KC_NO,  KC_UP, KC_NO,   KC_PSCR, KC_SLCK,  KC_PAUS,           KC_NO,    \
+      KC_NO,   KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2, RESET,KC_NO,KC_CALC,KC_NO,  KC_UP, KC_NO,   KC_PSCR, KC_SLCK,  KC_PAUS,           KC_NO,    \
       BL_STEP,   KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,KC_DEL, KC_END, KC_PGDN,KC_LEFT,  KC_DOWN,  KC_RIGHT,   KC_HOME, KC_PGUP,  KC_NO,             KC_ENT,    \
-      KC_LSFT, KC_LGUI,   KC_NO,   RGB_TOG,  RGB_MOD,KC_NO,  KC_NO,  KC_VOLD,KC_VOLU,KC_MUTE, KC_END,  KC_PGDN,  KC_RSFT, KC_NO,  KC_PGUP,      \
+      KC_LSFT, KC_NO,   KC_NO,   RGB_TOG,  RGB_MOD,KC_NO,  KC_NO,  KC_VOLD,KC_VOLU,KC_MUTE, KC_END,  KC_PGDN,  KC_RSFT, KC_PGUP,  KC_MS_BTN2,      \
       KC_LCTL, F(0), KC_LALT,                          KC_TRNS,                          KC_RGUI, F(0),     KC_HOME, KC_PGDOWN,KC_END),
-
 };
+
 
 // Custom Actions
 const uint16_t PROGMEM fn_actions[] = {
@@ -71,7 +70,15 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
   return MACRO_NONE;
 };
 
-// Loop
-void matrix_scan_user(void) {
-  // Empty
-};
+uint32_t layer_state_set_user(uint32_t state) {
+	switch (biton32(state)) {
+		case 0:
+			rgblight_setrgb(0x00, 0xFF, 0xFF); // Cyan on layer 0
+			break;
+		case 1:
+			rgblight_setrgb(0xFF, 0x00, 0xFF); // Purple on FN layer
+			break;
+	}
+	return state;
+}
+
