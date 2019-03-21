cf053c500507aa37d991efde0823f0bcc0c6d440
BK Bolisetty <bk@blitzkraft.me>
2019-03-20 22:58:36 -0500

    Add layer for mac layout

--- keymap.c
+++ keymap.c
@@ -5,6 +5,7 @@
 #define MOUSEKEY_TIME_TO_MAX       60
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+#define ____ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
@@ -26,9 +27,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSLS,   \
       CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,    \
       KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_NO, RSFT_T(KC_SLSH), KC_UP,  KC_MS_BTN1,      \
-      KC_LCTL, KC_LGUI, KC_LALT,                          LT(1, KC_SPC),                       KC_RALT, KC_RCTL,     KC_LEFT, KC_DOWN,  KC_RIGHT),
+      KC_LCTL, KC_LGUI, KC_LALT,                          LT(2, KC_SPC),                       KC_RALT, KC_RCTL,     KC_LEFT, KC_DOWN,  KC_RIGHT),
 
-/* 1: Function Layer
+/* 1: Mac Layer
+ * .----------------------------------------------------------------.
+ * |GrEsc| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = | \ | Bksp |  Escape on tap, tilde with shift
+ * |----------------------------------------------------------------|
+ * | Tab  | Q | W | E | R | T | Y | U | I | O | P | [ | ] |    \    |
+ * |----------------------------------------------------------------|
+ * |CL/CTRL | A | S | D | F | G | H | J | K | L | ; | ' |   Enter   |  Caps - CapsLock on Tap, CTRL on hold
+ * |----------------------------------------------------------------|
+ * |   Shift  | Z | X | C | V | B | N | M | , | . |Shft/Slsh| ↑ |LC |  Right Shift - Shift on hold, Slash on tap, Left click
+ * |----------------------------------------------------------------|
+ * | LCtrl | Optn | Cmnd |    Space/FN      | RAlt |Ctrl| ← | ↓ | → |
+ * '----------------------------------------------------------------'
+ */
+  LAYOUT_all(
+      KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_NO,    \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSLS,   \
+      CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,    \
+      KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_NO, RSFT_T(KC_SLSH), KC_UP,  KC_MS_BTN1,      \
+      KC_LCTL, KC_LALT, KC_LGUI,                          KC_TRNS,                       KC_RALT, KC_RCTL,     KC_LEFT, KC_DOWN,  KC_RIGHT),
+
+/* 2: Function Layer
  * .----------------------------------------------------------------.
  * | `   |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12| \ | Del  |
  * |----------------------------------------------------------------|
@@ -45,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,   KC_F14,    \
       KC_ESC,   KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2, RESET,KC_NO,KC_CALC,KC_NO,  KC_NO, KC_NO,   KC_PSCR, KC_SLCK,  KC_PAUS,           KC_NO,    \
       BL_STEP,   KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,KC_DEL, KC_END, KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT,   KC_HOME, KC_PGUP,  KC_NO,             KC_ENT,    \
-      KC_LSFT, KC_NO,   KC_NO,   RGB_TOG,  RGB_MOD,KC_NO,  KC_NO,  KC_VOLD,KC_VOLU,KC_MUTE, KC_END,  KC_PGDN,  KC_RSFT, KC_PGUP,  KC_MS_BTN2,      \
+      KC_LSFT, KC_NO,   KC_NO,   RGB_TOG,  RGB_MOD,KC_NO,  KC_NO,  KC_VOLD,KC_VOLU,KC_MUTE, KC_END,  KC_PGDN,  KC_RSFT, KC_PGUP,  TG(1),      \
       KC_LCTL, F(0), KC_LALT,                          KC_TRNS,                          KC_RGUI, F(0),     KC_HOME, KC_PGDOWN,KC_END),
 };
 
@@ -72,9 +93,12 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 uint32_t layer_state_set_user(uint32_t state) {
 	switch (biton32(state)) {
 		case 0:
-			rgblight_setrgb(0x00, 0xFF, 0xFF); // Cyan on layer 0
+			rgblight_setrgb(0x00, 0xAA, 0xAA); // Cyan on layer 0
 			break;
 		case 1:
+			rgblight_setrgb(0xAA, 0xAA, 0xAA); // Cyan on layer 0
+			break;
+		case 2:
 			rgblight_setrgb(0xFF, 0x00, 0xFF); // Purple on FN layer
 			break;
 	}
