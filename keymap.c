6732048c7bdec0780d274689fe0b32f377f07b28
BK Bolisetty <bk@blitzkraft.me>
2019-04-03 13:26:52 -0500

    Add CTRL for return key, on hold

--- keymap.c
+++ keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |----------------------------------------------------------------|
  * | Tab  | Q | W | E | R | T | Y | U | I | O | P | [ | ] |    \    |
  * |----------------------------------------------------------------|
- * |CL/CTRL | A | S | D | F | G | H | J | K | L | ; | ' |   Enter   |  Caps - CapsLock on Tap, CTRL on hold
+ * |CL/CTRL | A | S | D | F | G | H | J | K | L | ; | ' | Enter/CTRL|  Caps - CapsLock on Tap, CTRL on hold, Same with return
  * |----------------------------------------------------------------|
  * |   Shift  | Z | X | C | V | B | N | M | , | . |Shft/Slsh| ↑ |LC |  Right Shift - Shift on hold, Slash on tap, Left click
  * |----------------------------------------------------------------|
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT_all(
       KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_NO,    \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSLS,   \
-      CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,    \
+      CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO, CTL_T(KC_ENT),    \
       KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_NO, RSFT_T(KC_SLSH), KC_UP,  KC_MS_BTN1,      \
       KC_LCTL, KC_LGUI, KC_LALT,                          LT(2, KC_SPC),                       KC_RALT, KC_RCTL,     KC_LEFT, KC_DOWN,  KC_RIGHT),
 
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |----------------------------------------------------------------|
  * | Tab  | Q | W | E | R | T | Y | U | I | O | P | [ | ] |    \    |
  * |----------------------------------------------------------------|
- * |CL/CTRL | A | S | D | F | G | H | J | K | L | ; | ' |   Enter   |  Caps - CapsLock on Tap, CTRL on hold
+ * |CL/CTRL | A | S | D | F | G | H | J | K | L | ; | ' | Enter/CTRL|  Caps - CapsLock on Tap, CTRL on hold, Same with return
  * |----------------------------------------------------------------|
  * |   Shift  | Z | X | C | V | B | N | M | , | . |Shft/Slsh| ↑ |LC |  Right Shift - Shift on hold, Slash on tap, Left click
  * |----------------------------------------------------------------|
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT_all(
       KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_NO,    \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSLS,   \
-      CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,    \
+      CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,      KC_TRNS,    \
       KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_NO, RSFT_T(KC_SLSH), KC_UP,  KC_MS_BTN1,      \
       KC_LCTL, KC_LALT, KC_LGUI,                          KC_TRNS,                       KC_RALT, KC_RCTL,     KC_LEFT, KC_DOWN,  KC_RIGHT),
 
@@ -53,9 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .----------------------------------------------------------------.
  * | `   |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12| \ | Del  |
  * |----------------------------------------------------------------|
- * |  Esc |LC |MU |RC | * | T | Y | U | ↑ | O | P | [ | ] |    \    |  Left Click, Mouse Up, Right Click, * programming mode/RESET 
+ * |  Esc |LC |MU |RC | * | T | Y | U | ↑ | O | P | [ | ] | BL_STEP |  Left Click, Mouse Up, Right Click, * programming mode/RESET, BackLight Adj
  * |----------------------------------------------------------------|
- * |  BlAdj |ML |MD |MR | F | G | ← | ↑ | ↓ | → | ; | ' |   Enter   |  Backlight adjust; Mouse Left, Mouse Down, Mouse Right - Mouse movements, Arrow keys
+ * |CL/CTRL |ML |MD |MR | F | G | ← | ↑ | ↓ | → | ; | ' |   Enter   |  Mouse Left, Mouse Down, Mouse Right - Mouse movements, Arrow keys
  * |----------------------------------------------------------------|
  * |   Shift  | Z | ! | ? | V | B | N | M | , | . |Shft/Slsh|PU |RC |  Underglow (!) toggle on/off (?) cycle modes, Right Click
  * |----------------------------------------------------------------|
@@ -64,8 +64,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   LAYOUT_all(
       KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,   KC_F14,    \
-      KC_ESC,   KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2, RESET,KC_NO,KC_CALC,KC_NO,  KC_NO, KC_NO,   KC_PSCR, KC_SLCK,  KC_PAUS,           KC_NO,    \
-      BL_STEP,   KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,KC_DEL, KC_END, KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT,   KC_HOME, KC_PGUP,  KC_NO,             KC_ENT,    \
+      KC_ESC,   KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2, RESET,KC_NO,KC_CALC,KC_NO,  KC_NO, KC_NO,   KC_PSCR, KC_SLCK,  KC_PAUS,           BL_STEP,    \
+      KC_TRNS,   KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,KC_DEL, KC_END, KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT,   KC_HOME, KC_PGUP,  KC_NO,             KC_TRNS,    \
       KC_LSFT, KC_NO,   KC_NO,   RGB_TOG,  RGB_MOD,KC_NO,  KC_NO,  KC_VOLD,KC_VOLU,KC_MUTE, KC_END,  KC_PGDN,  KC_RSFT, KC_PGUP,  TG(1),      \
       KC_LCTL, F(0), KC_LALT,                          KC_TRNS,                          KC_RGUI, F(0),     KC_HOME, KC_PGDOWN,KC_END),
 };
@@ -96,7 +96,7 @@ uint32_t layer_state_set_user(uint32_t state) {
 			rgblight_setrgb(0x00, 0xAA, 0xAA); // Cyan on layer 0
 			break;
 		case 1:
-			rgblight_setrgb(0xAA, 0xAA, 0xAA); // Cyan on layer 0
+			rgblight_setrgb(0xAA, 0xAA, 0xAA); // White on layer 1
 			break;
 		case 2:
 			rgblight_setrgb(0xFF, 0x00, 0xFF); // Purple on FN layer
