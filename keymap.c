c2c269c95c82ae537306a4f50640ad1d3d6741e6
BK Bolisetty <bk@blitzkraft.me>
2019-09-13 12:23:35 -0500

    Add media play/pause toggle - Space+]
    Also, experiments with adding unicode symbols. Possibly working, but
    needs support on the OS (I think).

--- keymap.c
+++ keymap.c
@@ -7,6 +7,18 @@
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 #define ____ KC_TRNS
 
+enum unicode_names {
+    BANG,
+    IRONY,
+    SNEK
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [BANG]  = 0x203D,  // ‽
+    [IRONY] = 0x2E2E,  // ⸮
+    [SNEK]  = 0x1F40D, // 🐍
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
 /* 0: Base Layer
@@ -25,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT_all(
       KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_NO,    \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSLS,   \
-      CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO, CTL_T(KC_ENT),    \
+      CTL_T(KC_NO), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO, CTL_T(KC_ENT),    \
       KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_NO, RSFT_T(KC_SLSH), KC_UP,  KC_MS_BTN1,      \
       KC_LCTL, KC_LGUI, KC_LALT,                          LT(2, KC_SPC),                       KC_RALT, KC_RCTL,     KC_LEFT, KC_DOWN,  KC_RIGHT),
 
@@ -45,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT_all(
       KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_NO,    \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSLS,   \
-      CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,      KC_TRNS,    \
+      CTL_T(KC_NO), KC_A,    KC_S,    KC_D,   X(BANG),   X(IRONY),   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,      KC_TRNS,    \
       KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_NO, RSFT_T(KC_SLSH), KC_UP,  KC_MS_BTN1,      \
       KC_LCTL, KC_LALT, KC_LGUI,                          KC_TRNS,                       KC_RALT, KC_RCTL,     KC_LEFT, KC_DOWN,  KC_RIGHT),
 
@@ -64,8 +76,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   LAYOUT_all(
       KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,   KC_F14,    \
-      KC_ESC,   KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2, RESET,KC_NO,KC_CALC,KC_NO,  KC_NO, KC_NO,   KC_PSCR, KC_SLCK,  KC_PAUS,           BL_STEP,    \
-      KC_TRNS,   KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,KC_DEL, KC_END, KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT,   KC_HOME, KC_PGUP,  KC_NO,             KC_TRNS,    \
+      KC_ESC,   KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2, RESET,KC_NO,KC_CALC,KC_NO,  KC_NO, KC_NO,   KC_PSCR, KC_SLCK,  KC_MPLY,           BL_STEP,    \
+      CTL_T(KC_CAPS),   KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,X(BANG), X(IRONY), KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT,   KC_HOME, KC_PGUP,  KC_NO,             KC_TRNS,    \
+      //CTL_T(KC_NO), KC_A,    KC_S,    KC_D,   X(BANG),   X(IRONY),   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,      KC_TRNS,    
       KC_LSFT, KC_NO,   KC_NO,   RGB_TOG,  RGB_MOD,KC_NO,  KC_NO,  KC_VOLD,KC_VOLU,KC_KP_MINUS, KC_KP_PLUS,  KC_RSFT,  KC_RSFT, KC_PGUP,  TG(1),      \
       KC_LCTL, F(0), KC_LALT,                          KC_TRNS,                          KC_RGUI, F(0),     KC_HOME, KC_PGDOWN,KC_END),
 };
